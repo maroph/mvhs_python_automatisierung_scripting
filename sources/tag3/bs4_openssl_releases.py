0fe751927e3570c532761be0d94a0b98f1d80ba6
Manfred Rosenboom <maroph@gmail.com>
2025-09-18 14:46:50 +0200

    now print version and release date

--- sources/tag3/bs4_openssl_releases.py
+++ sources/tag3/bs4_openssl_releases.py
@@ -11,12 +11,16 @@ except Exception as e:
     print(e)
     sys.exit(1)
 
-div_h2_releases = soup.select("div[data-hpc] h2")
+# each <section> describes a version
+div_releases = soup.select("div[data-hpc] section")
 # print(f"div_releases: {div_releases}")
 
 count = 0
-for rel in div_h2_releases:
-    print(rel.text.strip())
+for rel in div_releases:
+    # <h2>: version name
+    # <relative-time datetime="...">: release datetime
+    # https://github.com/github/relative-time-element
+    print(f"{rel.find("h2").text.strip():<20} - {rel.find("relative-time").get("datetime")}")
     count += 1
     if count > 10:
         break
